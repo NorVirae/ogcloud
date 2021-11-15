3eff88f486d838b3ba8767a3aec066d28ee73178
NorVirae <norbertmbafrank@gmail.com>
2021-11-15 07:26:20 +0100

    added a class show case to section and configured its paragraph and the signup template

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,31 @@ import Navbar from '../components/Navbar';
 const Home = (props) => {
     return (<>
         <Navbar />
+        <section class="show-case">
+        <div class="container grid">
+            <div class="show-case-para">
+                <h1>THE BEST TOOL FOR DEVOPS!</h1>
+                <p>this is really the best tool for developers, it's providencial that developers
+                    who have less number of years of experience and less project on their portfolio
+                </p>
+                <button class="btn btn-outline">
+                    VISIT!
+                </button>
+            </div>
 
+            <div class="showcase-form card">
+                <form class="form-control">
+                    <p><h1>Hey Sign Up!</h1></p>
+                    <input placeholder="Name" name="name" type="text"/>
+                    <input placeholder="Email" name="email" type="email"/>
+                    <input placeholder="Company" name="company" type="text"/>
+                    <input class="btn btn-secondary" type="submit" />
+                </form>
+            </div>
+        </div>
+    </section>
+
+        
 
 
     </>)
