f86ccf5d4005104fd6374aa010d684f3a80a9fdd
NorVirae <norbertmbafrank@gmail.com>
2021-11-15 09:36:35 +0100

    added the components for platform  then styled them

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -4,6 +4,9 @@ import Navbar from "../components/Navbar";
 // image import
 
 import Daco from '../assets/images/Daco1.png'
+import Daco1 from '../assets/images/Daco2.png'
+
+
 
 
 
@@ -26,6 +29,22 @@ const Features = (props) => {
 
                 </div>
             </section>
+
+            {/* Platform */}
+
+    <section class="platform  bg-ash">
+        <div class="container grid">
+            <div class="server-text">
+                <h1 class="">Ogwugo Platform</h1>
+                <p class="">The Platform where all your cloudy dream comes to life to haunt you it makes you
+                    a better person at whatever you are doing currently.
+                </p>
+            </div>
+
+            <img src={Daco1} alt="servo" />
+
+        </div>
+    </section>
 
 
         {/*Footer  */}
