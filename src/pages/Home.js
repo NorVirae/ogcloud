31c990bc09aff31f018deadbb8d0b8767ee3f603
NorVirae <norbertmbafrank@gmail.com>
2021-11-15 08:51:30 +0100

    added the footer and its components nav and copyright and social media icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -131,7 +131,34 @@ const Home = (props) => {
                 </div>
     </section>
 
+    <footer class="footer">
+        <div class="container grid grid-3">
+            <div>
+                <h1>Ogwugo</h1>
+                &copy; Copyright 2021
+            </div>
 
+            <div>
+               <ul>
+                   <li><a href="#">Home</a></li>
+                   <li><a href="#">Documentation</a></li>
+                   <li><a href="#">Features</a></li>
+
+
+                </ul>
+
+            </div>
+
+            <div>
+                <span class="fab fa-github fa-2x"></span>
+                <span class="fab fa-facebook fa-2x"></span>
+                <span class="fab fa-instagram fa-2x"></span>
+                <span class="fab fa-twitter fa-2x"></span>
+
+            </div>
+
+        </div>
+    </footer>
 
     </>)
 }
