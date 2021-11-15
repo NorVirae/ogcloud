55f22c59ad7782f840b9ee69d3f7dbe4a675f926
NorVirae <norbertmbafrank@gmail.com>
2021-11-15 06:50:03 +0100

    just added the component of the Navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,24 @@ import {Link} from 'react-router-dom';
 
 
 const Home = (props) => {
-    return <div>Home</div>
+    return (<>
+        <div className="navbar">
+            <div className="container flex">
+            <h1>Ogwugo!</h1>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">Docs</Link></li>
+                    <li><Link to="/">Features</Link></li>
+
+                </ul>
+            </nav>
+            </div>
+        </div>
+
+
+
+    </>)
 }
 
 export default Home
