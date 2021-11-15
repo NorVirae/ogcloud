d9fab8991058515330418cbd24951c00f59deb75
NorVirae <norbertmbafrank@gmail.com>
2021-11-15 06:41:54 +0100

    just configured browser router for navigation mainly for react v6 and adding component folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
     
-    <div className="container">
-      This is seriously a fresh template
-    </div>
+    <>
+      <Routes>
+      <Route exact path={"/"} element={<Home />} />
+      </Routes>
+    </>
   
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import {Link} from 'react-router-dom';
+
+
+const Home = (props) => {
+    return <div>Home</div>
+}
+
+export default Home
