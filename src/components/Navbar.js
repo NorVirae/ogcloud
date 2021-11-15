260057b0960103b1f55ceb963149d53aa3308cdc
NorVirae <norbertmbafrank@gmail.com>
2021-11-15 07:22:34 +0100

    Moved Navbar to its own component

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+// this is the Navabar
+const Navbar = () => {
+
+        return (<div className="navbar">
+                    <div className="container flex">
+                    <h1><Link to="/">Ogwugo!</Link></h1>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/">Docs</Link></li>
+                            <li><Link to="/">Features</Link></li>
+
+                        </ul>
+                    </nav>
+                    </div>
+                </div>
+        )}
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,9 @@
 import {Link} from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const Home = (props) => {
     return (<>
-        <div className="navbar">
-            <div className="container flex">
-            <h1><Link to="/">Ogwugo!</Link></h1>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/">Docs</Link></li>
-                    <li><Link to="/">Features</Link></li>
-
-                </ul>
-            </nav>
-            </div>
-        </div>
+        <Navbar />
 
 
 
